a594fe972bfe6b83e97733e882f10b4226081347
Ivan Shalimov <iv.shalimoff@yandex.ru>
2024-02-02 19:57:32 +0400

    double text

--- app/src/main/java/com/ivanshalimov/composecoursesais/HomeScreen.kt
+++ app/src/main/java/com/ivanshalimov/composecoursesais/HomeScreen.kt
@@ -1,8 +1,10 @@
 package com.ivanshalimov.composecoursesais
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun HomeScreen() {
-
+    Text(text = "HomeScreen")
+    Text(text = "123456789")
 }
