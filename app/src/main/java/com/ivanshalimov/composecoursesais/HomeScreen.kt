5c32917500f82bb101cabe9ff0a2d28a0de82015
Ivan Shalimov <iv.shalimoff@yandex.ru>
2024-02-04 17:05:16 +0400

    modifier and arguments

--- app/src/main/java/com/ivanshalimov/composecoursesais/HomeScreen.kt
+++ app/src/main/java/com/ivanshalimov/composecoursesais/HomeScreen.kt
@@ -1,9 +1,16 @@
 package com.ivanshalimov.composecoursesais
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -11,7 +18,14 @@ fun HomeScreen(userName: String) {
     Text(
         text = "HomeScreen for $userName",
         fontSize = 32.sp,
-        color = Color.Green
+        color = Color.White,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .background(color = Color.Black)
+            //.width(120.dp),
+            .fillMaxWidth()
     )
     //Text(text = "123456789")
 }
