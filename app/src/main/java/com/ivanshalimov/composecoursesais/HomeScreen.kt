60385d8d448b0a581240ba3c205c51df8a39600f
Ivan Shalimov <iv.shalimoff@yandex.ru>
2024-02-04 16:50:33 +0400

    Work with preview and compose-function arguments

--- app/src/main/java/com/ivanshalimov/composecoursesais/HomeScreen.kt
+++ app/src/main/java/com/ivanshalimov/composecoursesais/HomeScreen.kt
@@ -2,9 +2,22 @@ package com.ivanshalimov.composecoursesais
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun HomeScreen() {
-    Text(text = "HomeScreen")
-    Text(text = "123456789")
+fun HomeScreen(userName: String) {
+    Text(
+        text = "HomeScreen for $userName",
+        fontSize = 32.sp,
+        color = Color.Green
+    )
+    //Text(text = "123456789")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen("Ivan")
 }
