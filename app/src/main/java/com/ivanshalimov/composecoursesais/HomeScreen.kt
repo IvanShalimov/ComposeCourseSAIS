ef3313a80de86714c5724b8a54cdb7201d438494
Ivan Shalimov <iv.shalimoff@yandex.ru>
2024-02-04 17:05:50 +0400

    clean some warnings

--- app/src/main/java/com/ivanshalimov/composecoursesais/HomeScreen.kt
+++ app/src/main/java/com/ivanshalimov/composecoursesais/HomeScreen.kt
@@ -2,7 +2,6 @@ package com.ivanshalimov.composecoursesais
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -10,7 +9,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
