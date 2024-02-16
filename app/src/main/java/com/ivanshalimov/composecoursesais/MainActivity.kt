0ed4455d70fa27603a8d5d8bc94744a7d0dc76df
Ivan Shalimov <iv.shalimoff@yandex.ru>
2024-02-16 22:21:06 +0400

    navigation - first iteration

--- app/src/main/java/com/ivanshalimov/composecoursesais/MainActivity.kt
+++ app/src/main/java/com/ivanshalimov/composecoursesais/MainActivity.kt
@@ -3,15 +3,55 @@ package com.ivanshalimov.composecoursesais
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       // val counter = mutableIntStateOf(0)
+        // val counter = mutableIntStateOf(0)
         //val checked = mutableStateOf(false)
         //val text = mutableStateOf("some text")
         setContent {
-            HomeScreenModifierOrder()
+            Column(modifier = Modifier.fillMaxSize()) {
+                var route by remember { mutableStateOf("home") }
+
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(16.dp)
+                ) {
+                    when (route) {
+                        "home" -> FirstScreen()
+                        "orders" -> SecondScreen()
+                        "users" -> ThirdScreen()
+                    }
+                }
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(text = "First", modifier = Modifier.clickable { route = "home" })
+                    Text(text = "Second", modifier = Modifier.clickable { route = "orders" })
+                    Text(text = "Third", modifier = Modifier.clickable { route = "users" })
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/com/ivanshalimov/composecoursesais/MultiScreen.kt
@@ -0,0 +1,19 @@
+package com.ivanshalimov.composecoursesais
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FirstScreen() {
+    Text(text = "Firs tScreen")
+}
+
+@Composable
+fun SecondScreen() {
+    Text(text = "Second Screen")
+}
+
+@Composable
+fun ThirdScreen() {
+    Text(text = "Third screen")
+}
