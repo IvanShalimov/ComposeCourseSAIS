80dcec4739c5894b1f26c35ad272d716e49524fd
Ivan Shalimov <iv.shalimoff@yandex.ru>
2024-02-18 12:59:05 +0400

    sideeffect - rememberCoroutineScope

--- app/src/main/java/com/ivanshalimov/composecoursesais/thirdscreen/ThirdActivity.kt
+++ app/src/main/java/com/ivanshalimov/composecoursesais/thirdscreen/ThirdActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Checkbox
@@ -15,11 +16,13 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ivanshalimov.composecoursesais.thirdscreen.ui.theme.ComposeCourseSAISTheme
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class ThirdActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,16 +33,24 @@ class ThirdActivity : ComponentActivity() {
                     var checkbox by remember { mutableStateOf(false) }
                     Checkbox(checked = checkbox, onCheckedChange = { checkbox = it })
                     if (checkbox) {
+                        val scope = rememberCoroutineScope()
+                        Text(text = "Click", modifier = Modifier.clickable {
+                            scope.launch { counter() }
+                        })
                         LaunchedEffect(key1 = Unit) {
-                            var count = 0
-                            while (true) {
-                                Log.d("Ivan", "count: ${count++}")
-                                delay(1000L)
-                            }
+                            counter()
                         }
                     }
                 }
             }
+        }
+    }
+
+    suspend fun counter() {
+        var count = 0
+        while (true) {
+            Log.d("Ivan", "count: ${count++}")
+            delay(1000L)
         }
     }
 }
