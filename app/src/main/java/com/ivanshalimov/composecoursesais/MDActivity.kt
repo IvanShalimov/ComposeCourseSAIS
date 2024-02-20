22b14d9cbd70b81decce7722237a00cd4d0aa9c9
Ivan Shalimov <iv.shalimoff@yandex.ru>
2024-02-20 09:31:47 +0400

    scaffold

--- app/src/main/java/com/ivanshalimov/composecoursesais/MDActivity.kt
+++ app/src/main/java/com/ivanshalimov/composecoursesais/MDActivity.kt
@@ -3,23 +3,83 @@ package com.ivanshalimov.composecoursesais
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.lightColorScheme
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
 import com.ivanshalimov.composecoursesais.ui.theme.ComposeCourseSAISTheme
 
 class MDActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeCourseSAISTheme {
-                MDHomeScreen1()
+                //MDHomeScreen1()
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text("TopAppBar")
+                            }
+                        )
+                    },
+                    bottomBar = {
+                        NavigationBar {
+                            NavigationBarItem(
+                                icon = { Icon(Icons.Filled.Home, contentDescription = null) },
+                                label = { Text("Home") },
+                                selected = true,
+                                onClick = { }
+                            )
+                            NavigationBarItem(
+                                icon = { Icon(Icons.Filled.Call, contentDescription = null) },
+                                label = { Text("Call") },
+                                selected = false,
+                                onClick = { }
+                            )
+                        }
+                    },
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = {  }) {
+                            Icon(
+                                imageVector = Icons.Filled.Add,
+                                contentDescription = null
+                            )
+                        }
+                    }
+                ) { innerPadding ->
+                    Box(
+                        modifier = Modifier.fillMaxSize().padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text("Content")
+                    }
+                }
             }
         }
     }
